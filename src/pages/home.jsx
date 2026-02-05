4cb1dbcba87b415f1f09cb2794bec2d05d424f90
Aswin Biju <aswinit072@gmail.com>
2026-02-05 23:40:18 +0530

    Added Images to project

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,5 @@
 import "./index.css"
-import Navbar from "../components/NavBar";
+import NavBar from "../components/NavBar";
 import Hero from "../sections/Hero";
 import Contact from "../sections/Contact"
 import TechStack from "../sections/TechStack";
@@ -7,7 +7,7 @@ import TechStack from "../sections/TechStack";
 const Home = () => {
     return (
         <>
-            <Navbar home={true} />
+            <NavBar home={true} />
             <Hero />
             <TechStack />
             <Contact />
